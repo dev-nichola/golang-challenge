ce76446d18a7d4a1da8b117a10e8ba1e53137bc6
Nichola Saputra <kerjasamakol@gmail.com>
2024-04-25 20:26:41 +0700

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dev-nichola/golang-challenge
+
+go 1.22.1
---
+++ hitung_luas.go
@@ -0,0 +1,13 @@
+package main
+
+func HitungLuasPersegi(sisi float64) float64 {
+	return sisi * sisi
+}
+
+func HitungLuasPersegiPanjang(panjang, lebar float64) float64 {
+	return panjang * lebar
+}
+
+func HitungLuasSegitiga(alas, tinggi float64) float64 {
+	return 0.5 * alas * tinggi
+}
